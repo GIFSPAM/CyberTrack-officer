e26c1d01aecd2c8c550bbb14cacb7bdc0cd9dac0
callme-ADHI <0adhithyakrishna@gmail.com>
2026-06-22 10:13:10 +0530

    Add nitro plugin back to fix Vercel SSR build

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    // TanStack Start must come BEFORE the react plugin
     tanstackStart({
       server: { entry: "server" }
     }),
-    // React plugin is required for dev mode HMR
     react(),
+    nitro()
   ],
 });
